7ceee942d519d1223429c24b8b75c14b6d78ea16
Muhammad Rizky Firmansyah <4956178-rizkyfirmansyah967@users.noreply.gitlab.com>
2019-11-15 14:06:30 +0700

    latihan day4 fungsi post

--- day_4/lib/array.mjs
+++ day_4/lib/array.mjs
@@ -5,11 +5,12 @@ const isString = data => {
 export const into_array = (data1, data2) => {
   let result
   let arr
-  let data_type = isString(data1)
+  let data_type1 = isString(data1)
+  let data_type2 = isString(data2)
 
-  if (data_type == true) {
-    arr = data1 + "," + data2
-    result = arr.split(",")
+  if (data_type1 == true && data_type2 == true) {
+    arr = data1 + "," + data2 // "gajahharimau"
+    result = arr.split(",") // "gajah","harimau"
   } else {
     result = "Not a String"
   }
