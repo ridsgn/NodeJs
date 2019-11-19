91deb467e4e5cc5ca09d9680d0cb3b9f745d5429
Muhammad Rizky Firmansyah <4956178-rizkyfirmansyah967@users.noreply.gitlab.com>
2019-11-19 13:23:01 +0700

    complete CRUD on NodeJs

--- day_6/routes/buku.routes.js
+++ day_6/routes/buku.routes.js
@@ -44,4 +44,43 @@ router.get("/:id", async (req, res) => {
 	})
 })
 
+router.put('/:id', async (req, res) => {
+	let { id } = req.params
+	let { id_buku, judul, penulis, thn_terbit, penerbit } = req.body
+
+	let update_data = {
+		id_buku,
+		judul,
+		penulis,
+		thn_terbit,
+		penerbit
+	}
+
+	try {
+		let data = await bookModel.findByIdAndUpdate(id, update_data)
+
+		return res.status(200).json({
+			status: "Success",
+			data,
+			message: "Data sukses di update"
+		})
+	} catch (err) {
+		return res.status(400).json({
+			status: "Error",
+			message: err.message
+		})
+	}
+})
+
+router.delete('/:id', async (req, res) => {
+	let id = req.params
+	let query = await bookModel.findOneAndDelete({ _id: id }).exec()
+
+	return res.status(200).json({
+		status: "Success",
+		query,
+		message: "Data berhasil di hapus"
+	})
+})
+
 module.exports = router
--- day_6/servers/db.js
+++ day_6/servers/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 let host = "mongodb://localhost:27017/day6"
 
 mongoose.connect(host, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 
 mongoose.set("useCreateIndex", true)
