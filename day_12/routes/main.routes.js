7f04e464183a6ec36f4be4bff974189d78c18bad
Muhammad Rizky Firmansyah <4956178-rizkyfirmansyah967@users.noreply.gitlab.com>
2019-11-26 16:37:14 +0700

    add update

--- day_12/routes/main.routes.js
+++ day_12/routes/main.routes.js
@@ -1,10 +1,12 @@
 const create = require("./User/create.routes")
 const detail = require("./User/detail.routes")
 const del = require("./User/destroy.routes")
+const update = require("./User/update.routes")
 
 const routes = (app) => {
     app.use("/delete", del)
     app.use("/create", create)
+    app.use("/update", update)
     app.use("/", detail)
 }
 
